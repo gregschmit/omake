4c138dbf7f756f5309270ae6c07b21026dcda212
Matheus Xavier <mxavierdev@gmail.com>
2023-03-01 23:54:24 -0300

    Make crate::args::args_to_submake_str more robust.

--- src/args.rs
+++ src/args.rs
@@ -15,16 +15,16 @@ const BUILD_MODE: &str = if cfg!(debug_assertions) {
 /// Represents the `clap`-based arguments provided by this binary.
 #[derive(Clone, Debug, Parser)]
 #[clap(
-    name = "make (oxidized)",
-    version,
-    about,
-    after_help = formatcp!(
-        "License:     {}\nSource:      {}\nVersion:     {}\nBuild type:  {}", 
-        env!("CARGO_PKG_LICENSE"),
-        env!("CARGO_PKG_REPOSITORY"),
-        env!("CARGO_PKG_VERSION"),
-        BUILD_MODE
-    ),
+name = "make (oxidized)",
+version,
+about,
+after_help = formatcp ! (
+"License:     {}\nSource:      {}\nVersion:     {}\nBuild type:  {}",
+env ! ("CARGO_PKG_LICENSE"),
+env ! ("CARGO_PKG_REPOSITORY"),
+env ! ("CARGO_PKG_VERSION"),
+BUILD_MODE
+),
 )]
 pub struct Args {
     /// Target(s) (if none specifired, use first regular target).
@@ -57,10 +57,10 @@ pub struct Args {
 
     /// Don't execute recipes; just print them.
     #[arg(
-        short = 'n',
-        long = "just-print",
-        visible_alias("dry-run"),
-        visible_alias("recon")
+    short = 'n',
+    long = "just-print",
+    visible_alias("dry-run"),
+    visible_alias("recon")
     )]
     pub just_print: bool,
 
@@ -70,11 +70,11 @@ pub struct Args {
 
     /// Consider FILE to be very new to simulate "what if" it changed.
     #[arg(
-        short = 'W',
-        long = "what-if",
-        value_name = "FILE",
-        visible_alias("new-file"),
-        visible_alias("assume-new")
+    short = 'W',
+    long = "what-if",
+    value_name = "FILE",
+    visible_alias("new-file"),
+    visible_alias("assume-new")
     )]
     pub new_file: Vec<String>,
 
@@ -84,35 +84,43 @@ pub struct Args {
 }
 
 /// Converts the arguments to a string that can be passed to a sub-make invocation.
-pub fn args_to_submake_str() -> String {
+pub(crate) fn args_to_submake_str() -> String {
+    let mut single_flags = Vec::new();
+    let mut arguments = Vec::new();
     // Rudimetary MAKEFLAGS parsing, the '-j' flag handling is not implemented yet.
     // TODO: This should probably be a `Result` instead of a `panic!`.
     // NOTE: Maybe change the way this is done to a pure IPC solution? when sub-make is used?
-    let args = std::env::args()
+    std::env::args()
         .collect::<Vec<_>>()
         .iter()
-        .map(|arg| {
+        .for_each(|arg| {
             let mut arg_mod = arg.clone();
-
             // If the argument contains a space, we need to quote it.
-            if arg_mod.contains(' ') {
-                arg_mod = format!("\"{}\"", arg_mod);
+            if arg.contains(' ') {
+                arg_mod = format!("\"{}\"", arg);
+            }
+
+            if SUBMAKE_FORBIDDEN_FLAGS.contains(&arg_mod.as_str()) {
+                return;
             }
 
             if arg_mod.starts_with("--") {
-                arg_mod = arg_mod[2..].to_string();
+                arguments.push(arg[2..].to_string());
             } else if arg_mod.starts_with('-') {
-                arg_mod = arg_mod[1..].to_string();
+                single_flags.push(arg[1..].to_string());
             }
+        });
 
-            // remove special cases
-            if SUBMAKE_FORBIDDEN_FLAGS.contains(&arg_mod.as_str()) {
-                arg_mod = "".to_string();
-            }
-            arg_mod
-        })
-        .collect::<Vec<_>>()
-        .join(" ");
+    let mut result = "-".to_string();
+    if single_flags.is_empty() {
+        result = "".to_string();
+    } else {
+        result.insert_str(1, &single_flags.join(""));
+    }
 
-    format!("-{}", args)
+    for argument in arguments {
+        result.push_str(&format!(" --{}", argument));
+    }
+
+    result
 }
