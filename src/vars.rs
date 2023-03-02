d6de24e1b75c8f0b71dc96c01d6a7fd0d3bd34a7
Matheus Xavier <mxavierdev@gmail.com>
2023-03-01 23:22:30 -0300

    Fix issues with #8 pointed by @gregschmit.

--- src/vars.rs
+++ src/vars.rs
@@ -9,94 +9,7 @@
 
 use std::collections::HashMap;
 
-use lazy_static::lazy_static;
-
-const BAD_VARIABLE_CHARS: [char; 3] = [':', '#', '='];
-const DEFAULT_SUFFIXES: [&str; 13] = [
-    ".C", ".F", ".S", ".c", ".cc", ".cpp", ".def", ".f", ".m", ".mod", ".p", ".r", ".s",
-];
-
-/// List of variables where setting the value to blank means to reset it to the default value.
-const BLANK_MEANS_DEFAULT_VARS: [&str; 1] = [".RECIPEPREFIX"];
-
-lazy_static! {
-    /// Variables which are set in a non-recursive context by default, and can be overridden by the
-    /// environment. `SHELL` is not included, since it cannot be overridden by the environment, unless
-    /// explicitly directed to by `-e`.
-    static ref DEFAULT_VARS: HashMap<String, String> = HashMap::from(
-        [
-            (".RECIPEPREFIX", "\t"),
-            (".SHELLFLAGS", "-c"),
-            ("AR", "ar"),
-            ("ARFLAGS", "rv"),
-            ("AS", "as"),
-            ("CC", "cc"),
-            ("CO", "co"),
-            ("CTANGLE", "ctangle"),
-            ("CWEAVE", "cweave"),
-            ("CXX", "c++"),
-            ("FC", "f77"),
-            ("GET", "get"),
-            ("LD", "ld"),
-            ("LEX", "lex"),
-            ("LINT", "lint"),
-            ("M2C", "m2c"),
-            ("OBJC", "cc"),
-            ("PC", "pc"),
-            ("RM", "rm -f"),
-            ("TANGLE", "tangle"),
-            ("TEX", "tex"),
-            ("WEAVE", "weave"),
-            ("YACC", "yacc"),
-        ].map(|(k, v)| (k.to_string(), v.to_string()))
-    );
-}
-
-/// Variables which are set in a recursive context by default, and can be overridden by the
-/// environment.
-#[rustfmt::skip]
-const DEFAULT_RECURSIVE_VARS: [(&str, &str); 36] = [
-    ("OUTPUT_OPTION", "-o $@"),
-
-    // Compiler definitions.
-    ("CPP", "$(CC) -E"),
-    ("F77", "$(FC)"),
-    ("F77FLAGS", "$(FFLAGS)"),
-    ("LEX.m", "$(LEX) $(LFLAGS) -t"),
-    ("YACC.m", "$(YACC) $(YFLAGS)"),
-    ("YACC.y", "$(YACC) $(YFLAGS)"),
-
-    // Implicit rule definitions.
-    ("COMPILE.C", "$(COMPILE.cc)"),
-    ("COMPILE.F", "$(FC) $(FFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.S", "$(CC) $(ASFLAGS) $(CPPFLAGS) $(TARGET_MACH) -c"),
-    ("COMPILE.c", "$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.cc", "$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.cpp", "$(COMPILE.cc)"),
-    ("COMPILE.def", "$(M2C) $(M2FLAGS) $(DEFFLAGS) $(TARGET_ARCH)"),
-    ("COMPILE.f", "$(FC) $(FFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.m", "$(OBJC) $(OBJCFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.mod", "$(M2C) $(M2FLAGS) $(MODFLAGS) $(TARGET_ARCH)"),
-    ("COMPILE.p", "$(PC) $(PFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.r", "$(FC) $(FFLAGS) $(RFLAGS) $(TARGET_ARCH) -c"),
-    ("COMPILE.s", "$(AS) $(ASFLAGS) $(TARGET_MACH)"),
-    ("LINK.C", "$(LINK.cc)"),
-    ("LINK.F", "$(FC) $(FFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.S", "$(CC) $(ASFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_MACH)"),
-    ("LINK.c", "$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.cc", "$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.cpp", "$(LINK.cc)"),
-    ("LINK.f", "$(FC) $(FFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.m", "$(OBJC) $(OBJCFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.o", "$(CC) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.p", "$(PC) $(PFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.r", "$(FC) $(FFLAGS) $(RFLAGS) $(LDFLAGS) $(TARGET_ARCH)"),
-    ("LINK.s", "$(CC) $(ASFLAGS) $(LDFLAGS) $(TARGET_MACH)"),
-    ("LINT.c", "$(LINT) $(LINTFLAGS) $(CPPFLAGS) $(TARGET_ARCH)"),
-    ("PREPROCESS.F", "$(FC) $(FFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -F"),
-    ("PREPROCESS.S", "$(CC) -E $(CPPFLAGS)"),
-    ("PREPROCESS.r", "$(FC) $(FFLAGS) $(RFLAGS) $(TARGET_ARCH) -F"),
-];
+const DEFAULT_RECIPE_PREFIX: char = '\t';
 
 /// Represents the "raw" environment coming from the OS.
 pub type Env = HashMap<String, String>;
@@ -113,29 +26,16 @@ pub struct Var {
 pub struct Vars {
     map: HashMap<String, Var>,
 
-    /// Variable to return when a variable is not found. This is allocated during initialization to
-    /// prevent multiple blank allocations in the map and lifetime tracking.
+    // Heap-allocated "constant" `Var` objects, setup during initialization, designed to reduce
+    // multiple allocations and lifetime tracking.
     blank: Var,
     default_recipe_prefix: Var,
-    make_var: Var,
-    makeargs_var: Var,
 }
 
 impl Vars {
     /// Primary interface for configuring a new instance. We also create some cached values that
     /// live for the lifetime of this instance to reduce the number of allocations.
     pub fn new<const N: usize>(init: [(&str, &str); N]) -> Self {
-        // Get the executable path for the `MAKE` variable.
-        let exe_path = match std::env::current_exe() {
-            // TODO: This should probably be a `Result` instead of a `panic!`.
-            // Try to canonicalize the path, but if that fails we panic because
-            // this means something is very wrong with the system.
-            Ok(path) => path.canonicalize().unwrap().to_string_lossy().into_owned(),
-            Err(_) => panic!("Unable to get executable path!"),
-        };
-
-        
-
         let mut vars = Self {
             map: HashMap::new(),
             blank: Var {
@@ -146,34 +46,16 @@ impl Vars {
                 value: DEFAULT_RECIPE_PREFIX.to_string(),
                 recursive: false,
             },
-            make_var: Var {
-                value: exe_path,
-                recursive: false,
-            },
-            makeargs_var: Var {
-                value: crate::args::args_to_submake_str(),
-                recursive: false,
-            },
         };
 
-        // Set default vars.
-        for (k, v) in DEFAULT_VARS.iter() {
-            vars.set(k, v, false).unwrap();
-        }
+        let exe_path = std::env::current_exe()
+            .expect("failed to get executable path")
+            .canonicalize().expect("Failed to canonicalize executable path")
+            .to_string_lossy().into_owned();
 
-        // Set default recursive vars.
-        for (k, v) in DEFAULT_RECURSIVE_VARS {
-            vars.set(k, v, true).unwrap();
-        }
-
-        // Set `SHELL` to `/bin/sh` by default.
-        vars.set("SHELL", "/bin/sh", false).unwrap();
-
-        // Set default `SUFFIXES` and `.SUFFIXES`.
-        vars.set("SUFFIXES", &DEFAULT_SUFFIXES.join(" "), false)
-            .unwrap();
-        vars.set(".SUFFIXES", &DEFAULT_SUFFIXES.join(" "), false)
-            .unwrap();
+        vars.set("MAKE", &exe_path, false).expect("Failed to set MAKE variable");
+        vars.set("MAKEFLAGS", &crate::args::args_to_submake_str(), false)
+            .expect("Failed to set MAKEFLAGS variable");
 
         // Use `set` to initialize data.
         for (k, v) in init {
@@ -183,15 +65,12 @@ impl Vars {
         vars
     }
 
-    /// Public interface for getting variables. For unknown keys, the `blank` object is returned. We
-    /// should try to keep this interface as fast/simple as possible since it's used far more often
-    /// than `set` (e.g., used for each line to check for recipe prefix).
+    /// Public interface for getting variables. For unknown keys, the `blank` object is returned,
+    /// and some special keys have default values.
     pub fn get(&self, k: impl AsRef<str>) -> &Var {
         let k = k.as_ref().trim();
         match k {
             // Special variables.
-            "MAKE" => &self.make_var,
-            "MAKEFLAGS" => &self.makeargs_var,
             ".RECIPEPREFIX" => match self.map.get(k) {
                 None => &self.default_recipe_prefix,
                 Some(var) => {
@@ -207,31 +86,28 @@ impl Vars {
                 None => &self.blank,
                 Some(var) => var,
             },
-
-        match self.map.get(k) {
-            None => &self.blank,
-            Some(var) => var,
         }
     }
 
     /// Public interface for setting variables.
     pub fn set<S: Into<String>>(&mut self, k: S, v: S, recursive: bool) -> Result<(), String> {
         let k = k.into().trim().to_string();
-        let mut v = v.into();
+        let v = v.into();
 
-        // Do not insert bad variable names.
+        // Variable names must not include whitespace or any chars in the set: `:#=`.
         for ch in k.chars() {
             if ch.is_whitespace() {
                 return Err("Variable contains whitespace.".to_string());
             }
 
-            if BAD_VARIABLE_CHARS.contains(&ch) {
-                return Err(format!("Variable contains bad character '{}'.", ch));
+            if let Some(bad_char) = match ch {
+                ':' => Some(':'),
+                '#' => Some('#'),
+                '=' => Some('='),
+                _ => None,
+            } {
+                return Err(format!("Variable contains bad character '{}'.", bad_char));
             }
-        }
-
-        if BLANK_MEANS_DEFAULT_VARS.contains(&&k[..]) && v.is_empty() {
-            v = DEFAULT_VARS.get(&k).unwrap().to_string();
         }
 
         self.map.insert(
@@ -248,13 +124,7 @@ impl Vars {
 impl From<Env> for Vars {
     fn from(env: Env) -> Self {
         let mut vars = Self::new([]);
-
         for (k, v) in env {
-            // Do not load `SHELL` from the environment.
-            if k == "SHELL" {
-                continue;
-            }
-
             vars.map.insert(
                 k,
                 Var {
@@ -277,11 +147,6 @@ mod tests {
         let vars = Vars::new([("A", "B")]);
         assert_eq!(vars.get("A").value, "B");
         assert_eq!(vars.get("B").value, "");
-        assert_eq!(vars.get("SHELL").value, "/bin/sh");
-        assert_eq!(
-            vars.get("COMPILE.c").value,
-            "$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c"
-        );
     }
 
     #[test]
